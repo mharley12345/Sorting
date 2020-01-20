7b568a6a2871af153ed84995b733c37380f42e82
Mike Harley <mharley12345@gmail.com>
2020-01-20 10:16:01 -0800

    :tada init commit and selection_sort complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,6 +4,10 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        for e in range(cur_index+1, len(arr)):
+            if arr[cur_index] > arr[e]:
+                cur_index = e
+        arr[smallest_index], arr[cur_index] = arr[cur_index],arr[smallest_index]
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
              
